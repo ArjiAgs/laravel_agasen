d1c8c4ce6141b1d4023bba01a40a78ff89f318bf
ArjiAgs <arji.agasen@lorma.edu>
2023-04-13 15:03:47 +0800

    Update message

---
+++ resources/views/viewdetail.blade.php
@@ -0,0 +1,4 @@
+<h5>Product Name: {{$product->product_name}}</h5>
+<li>Price: {{$product->unit_price}}</li>
+<li>Product Unit: {{$product->unit}} </li>
+<li>Product Quantity: {{$product->quantity}}</li>
